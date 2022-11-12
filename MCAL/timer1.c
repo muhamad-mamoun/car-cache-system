90b3509db21a8f2c1e3cbd63fb96f4029ac3518f
Muhamad Mamoun <muhamadmamoun@gmail.com>
2022-11-13 00:21:32 +0200

    Timer1 driver

---
+++ MCAL/timer1.c
@@ -0,0 +1,178 @@
+/*
+===========================================================================================================
+Author       : Mamoun
+Module       : Timer1
+File Name    : timer1.c
+Date Created : Nov 1, 2022
+Description  : Source file for the AVR Timer1 driver.
+===========================================================================================================
+*/
+
+
+/*===========================================================================================================
+                                               < Includes >
+===========================================================================================================*/
+
+#include "timer1.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+/*===========================================================================================================
+                                           < Global Variables >
+===========================================================================================================*/
+
+static TIMER1_clockPrescalerType g_last_prescaler_value = TIMER1_NO_CLOCK;
+volatile static void (*g_ptr2callbackfunc)(void) = NULL_PTR;
+
+/*===========================================================================================================
+                                      < Private Functions Prototypes >
+===========================================================================================================*/
+
+ISR(TIMER1_OVF_vect)
+{
+	if(g_ptr2callbackfunc != NULL_PTR)
+	{
+		(*g_ptr2callbackfunc)();
+	}
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	if(g_ptr2callbackfunc != NULL_PTR)
+	{
+		(*g_ptr2callbackfunc)();
+	}
+}
+
+ISR(TIMER1_COMPB_vect)
+{
+	if(g_ptr2callbackfunc != NULL_PTR)
+	{
+		(*g_ptr2callbackfunc)();
+	}
+}
+
+/*===========================================================================================================
+                                          < Functions Definitions >
+===========================================================================================================*/
+
+/*===========================================================================================================
+ * [Function Name] : TIMER1_OVERFLOW_init
+ * [Description]   : Initialize Timer1 to run in Overflow mode with specific configurations.
+ * [Arguments]     : <a_ptr2configurations>      -> Pointer to structure.
+ *                                                  This structure have the required configurations.
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void TIMER1_OVERFLOW_init(const TIMER1_OVERFLOW_configurationsType* a_ptr2configurations)
+{
+	TIMER1_Deinit();                   /* De-Initialize Timer1 to reset the previous configurations. */
+	TCCR1A |= (1<<FOC1A);                                       /* Specifies a non-PWM mode.         */
+	TCCR1A |= (1<<FOC1B);                                       /* Specifies a non-PWM mode.         */
+	TIMSK |= (1<<TOIE1);                                        /* Enable Overflow Interrupt.        */
+	TCNT1 = a_ptr2configurations->initial_value;                /* Set Timer1 initial value.         */
+	TCCR1B = (TCCR1B & 0XF8)|(a_ptr2configurations->prescaler); /* Select the required prescaler.    */
+
+	/* Store the prescaler value to be used in resume function */
+	g_last_prescaler_value = a_ptr2configurations->prescaler;
+}
+
+/*===========================================================================================================
+ * [Function Name] : TIMER1_CTC_init
+ * [Description]   : Initialize Timer1 to run in CTC mode with specific configurations.
+ * [Arguments]     : <a_ptr2configurations>      -> Pointer to structure.
+ *                                                  This structure have the required configurations.
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void TIMER1_CTC_init(const TIMER1_CTC_configurationsType* a_ptr2configurations)
+{
+	TIMER1_Deinit();                   /* De-Initialize Timer1 to reset the previous configurations. */
+	TCCR1A |= (1<<FOC1A);                                       /* Specifies a non-PWM mode.         */
+	TCCR1A |= (1<<FOC1B);                                       /* Specifies a non-PWM mode.         */
+	TCCR1B |= (1<<WGM12);                                       /* Set Timer1 to CTC mode.           */
+	TIMSK |= (1<<OCIE1A);                                       /* Enable Compare Match Interrupt.   */
+	OCR1A = a_ptr2configurations->compare_value;                /* Set Timer1 compare value.         */
+	TCNT1 = a_ptr2configurations->initial_value;                /* Set Timer1 initial value.         */
+	TCCR1B = (TCCR1B & 0XF8)|(a_ptr2configurations->prescaler); /* Select the required prescaler.    */
+
+	/* Store the prescaler value to be used in resume function */
+	g_last_prescaler_value = a_ptr2configurations->prescaler;
+}
+
+/*===========================================================================================================
+ * [Function Name] : TIMER1_PWM_init
+ * [Description]   : Initialize Timer1 to run in Fast PWM mode with specific configurations.
+ * [Arguments]     : <a_ptr2configurations>      -> Pointer to structure.
+ *                                                  This structure have the required configurations.
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void TIMER1_PWM_init(const TIMER1_PWM_configurationsType* a_ptr2configurations)
+{
+	/* Calculate the required PWM frequency and duty-cycle based on the configurations. */
+	uint16 OCR1A_value = (F_CPU/(a_ptr2configurations->frequency*a_ptr2configurations->prescaler_divider))-1;
+	uint16 OCR1B_value = (uint16)((uint32)a_ptr2configurations->duty_cycle*655.35);
+
+	TIMER1_Deinit();                   /* De-Initialize Timer1 to reset the previous configurations. */
+	TCCR1A = (TCCR1A & 0X3F)|((a_ptr2configurations->OC1A_output_mode)<<6);        /* Set OC1A mode. */
+	TCCR1A = (TCCR1A & 0XCF)|((a_ptr2configurations->OC1B_output_mode)<<4);        /* Set OC1B mode. */
+	TCCR1A |= (1<<WGM10);                                       /* Set Timer1 to Fast PWM mode.      */
+	TCCR1A |= (1<<WGM11);                                       /* Set Timer1 to Fast PWM mode.      */
+	TCCR1B |= (1<<WGM12);                                       /* Set Timer1 to Fast PWM mode.      */
+	TCCR1B |= (1<<WGM13);                                       /* Set Timer1 to Fast PWM mode.      */
+	OCR1A = OCR1A_value;                                        /* Set the required PWM frequency.   */
+	OCR1B = OCR1B_value;                                        /* Set the required PWM duty-cycle.  */
+	TCCR1B = (TCCR1B & 0XF8)|(a_ptr2configurations->prescaler); /* Select the required prescaler.    */
+
+	/* Store the prescaler value to be used in resume function */
+	g_last_prescaler_value = a_ptr2configurations->prescaler;
+}
+
+/*===========================================================================================================
+ * [Function Name] : TIMER1_setCallBack
+ * [Description]   : Save the address of the call back function in a global variable.
+ * [Arguments]     : <a_ptr2callbackfunc>      -> Pointer points to the call-back function.
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void TIMER1_setCallBack(void(*a_ptr2callbackfunc)(void))
+{
+	g_ptr2callbackfunc = a_ptr2callbackfunc;
+}
+
+/*===========================================================================================================
+ * [Function Name] : TIMER1_pause
+ * [Description]   : Disable TIMER1 counting.
+ * [Arguments]     : The function takes no arguments.
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void TIMER1_pause(void)
+{
+	/* Disable Clock Source To Pause Timer1. */
+	TCCR1B = (TCCR1B & 0XF8);
+}
+
+/*===========================================================================================================
+ * [Function Name] : TIMER1_resume
+ * [Description]   : Take TIMER1 back to continue the previous operation.
+ * [Arguments]     : The function takes no arguments.
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void TIMER1_resume(void)
+{
+	/* Set the previous prescaler to resume Timer1. */
+	TCCR1B = (TCCR1B & 0XF8)|(g_last_prescaler_value);
+}
+
+/*===========================================================================================================
+ * [Function Name] : TIMER1_Deinit
+ * [Description]   : De-Initialize TIMER1 by resetting all registers.
+ * [Arguments]     : The function takes no arguments.
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void TIMER1_Deinit(void)
+{
+	TIMSK = (TIMSK & 0XC3);
+	TCCR1A = 0;
+	TCCR1B = 0;
+	OCR1A = 0;
+	OCR1B = 0;
+	TCNT1 = 0;
+}
---
+++ MCAL/timer1.h
@@ -0,0 +1,135 @@
+/*
+===========================================================================================================
+Author       : Mamoun
+Module       : Timer1
+File Name    : timer1.h
+Date Created : Nov 1, 2022
+Description  : Header file for the AVR Timer1 driver.
+===========================================================================================================
+*/
+
+
+#ifndef TIMER1_H_
+#define TIMER1_H_
+
+/*===========================================================================================================
+                                               < Includes >
+===========================================================================================================*/
+
+#include "../OTHERS/std_types.h"
+
+/*===========================================================================================================
+                                       < User-defined Data Types >
+===========================================================================================================*/
+
+typedef enum
+{
+	NORMAL,TOGGLE,INVERTING,NON_INVERTING
+}TIMER1_compareOutputModeType;
+
+typedef enum
+{
+	TIMER1_NO_CLOCK,
+	TIMER1_FCPU_1,
+	TIMER1_FCPU_8,
+	TIMER1_FCPU_64,
+	TIMER1_FCPU_256,
+	TIMER1_FCPU_1024,
+	TIMER1_COUNTER_FALLING,
+	TIMER1_COUNTER_RISING
+}TIMER1_clockPrescalerType;
+
+typedef struct
+{
+	uint16 initial_value;
+	TIMER1_clockPrescalerType prescaler;
+}TIMER1_OVERFLOW_configurationsType;
+
+typedef struct
+{
+	uint16 initial_value;
+	uint16 compare_value;
+	TIMER1_clockPrescalerType prescaler;
+}TIMER1_CTC_configurationsType;
+
+typedef struct
+{
+	uint8 duty_cycle;
+	uint32 frequency;
+	uint16 prescaler_divider;              /* The value of the devider (1, 8, 64, 256, or 1024). */
+	TIMER1_clockPrescalerType prescaler;
+	TIMER1_compareOutputModeType OC1A_output_mode;
+	TIMER1_compareOutputModeType OC1B_output_mode;
+}TIMER1_PWM_configurationsType;
+
+/*===========================================================================================================
+                                         < Functions Prototypes >
+===========================================================================================================*/
+
+
+/*===========================================================================================================
+ * [Function Name] : TIMER1_OVERFLOW_init
+ * [Description]   : Initialize Timer1 to run in Overflow mode with specific configurations.
+ * [Arguments]     : <a_ptr2configurations>      -> Pointer to structure.
+ *                                                  This structure have the required configurations.
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void TIMER1_OVERFLOW_init(const TIMER1_OVERFLOW_configurationsType* a_ptr2configurations);
+
+
+/*===========================================================================================================
+ * [Function Name] : TIMER1_CTC_init
+ * [Description]   : Initialize Timer1 to run in CTC mode with specific configurations.
+ * [Arguments]     : <a_ptr2configurations>      -> Pointer to structure.
+ *                                                  This structure have the required configurations.
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void TIMER1_CTC_init(const TIMER1_CTC_configurationsType* a_ptr2configurations);
+
+
+/*===========================================================================================================
+ * [Function Name] : TIMER1_PWM_init
+ * [Description]   : Initialize Timer1 to run in Fast PWM mode with specific configurations.
+ * [Arguments]     : <a_ptr2configurations>      -> Pointer to structure.
+ *                                                  This structure have the required configurations.
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void TIMER1_PWM_init(const TIMER1_PWM_configurationsType* a_ptr2configurations);
+
+
+/*===========================================================================================================
+ * [Function Name] : TIMER1_setCallBack
+ * [Description]   : Save the address of the call back function in a global variable.
+ * [Arguments]     : <a_ptr2callbackfunc>      -> Pointer points to the call-back function.
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void TIMER1_setCallBack(void(*a_ptr2callbackfunc)(void));
+
+
+/*===========================================================================================================
+ * [Function Name] : TIMER1_pause
+ * [Description]   : Disable TIMER1 counting.
+ * [Arguments]     : The function takes no arguments.
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void TIMER1_pause(void);
+
+
+/*===========================================================================================================
+ * [Function Name] : TIMER1_resume
+ * [Description]   : Take TIMER1 back to continue the previous operation.
+ * [Arguments]     : The function takes no arguments.
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void TIMER1_resume(void);
+
+
+/*===========================================================================================================
+ * [Function Name] : TIMER1_Deinit
+ * [Description]   : De-Initialize TIMER1 by resetting all registers.
+ * [Arguments]     : The function takes no arguments.
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void TIMER1_Deinit(void);
+
+#endif /* TIMER1_H_ */
