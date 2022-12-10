0a547643f57701cdc797d31ee92a7b0e54e1734c
Muhamad Mamoun <muhamadmamoun@gmail.com>
2022-12-10 19:50:08 +0200

    RFID driver

--- HAL/rfid.c
+++ HAL/rfid.c
@@ -1,10 +1,10 @@
 /*
 ===========================================================================================================
 Author       : Mamoun
-Module       : 
+Module       : RFID
 File Name    : rfid.c
 Date Created : Nov 27, 2022
-Description  : 
+Description  : Source file for the RFID driver (using UART).
 ===========================================================================================================
 */
 
@@ -13,19 +13,14 @@ Description  :
                                                < Includes >
 ===========================================================================================================*/
 
-
+#include "rfid.h"
+#include "../MCAL/uart.h"
 
 /*===========================================================================================================
                                            < Global Variables >
 ===========================================================================================================*/
 
-
-
-/*===========================================================================================================
-                                      < Private Functions Prototypes >
-===========================================================================================================*/
-
-
+UART_configurationsType UART_configurations = {UART_SINGLE_STOP_BIT,UART_PARITY_DISABLED,128000};
 
 /*===========================================================================================================
                                           < Functions Definitions >
@@ -38,3 +33,104 @@ Description  :
  *                   <>      -> 
  * [return]        : The function returns void.
  ==========================================================================================================*/
+void RFID_init(void)
+{
+	UART_init(&UART_configurations);
+	RFID_wakeUpDelay();
+	RFID_powerDownMode_ON();
+	RFID_writeData(RFID_TEST_PIN_EN_REG,0X00); /* Disable MX and DTRQ */
+	RFID_writeData(RFID_SERIAL_SPEED_REG,0X74); /* Change the default BR to 128 KBR */
+}
+
+/*===========================================================================================================
+ * [Function Name] :
+ * [Description]   :
+ * [Arguments]     : <>      ->
+ *                   <>      ->
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+uint8 RFID_readData(uint8 a_address)
+{
+	uint8 data;
+	UART_sendByte((a_address & 0X3F)|(1<<7));
+	data = UART_receiveByte();
+	return data;
+}
+
+/*===========================================================================================================
+ * [Function Name] :
+ * [Description]   :
+ * [Arguments]     : <>      ->
+ *                   <>      ->
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void RFID_writeData(uint8 a_address, uint8 a_data)
+{
+	UART_sendByte((a_address & 0X3F));
+	UART_sendByte(a_data);
+}
+
+/*===========================================================================================================
+ * [Function Name] :
+ * [Description]   :
+ * [Arguments]     : <>      ->
+ *                   <>      ->
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void RFID_wakeUpDelay(void)
+{
+	uint8 data = RFID_readData(0X00); /* Dummy Byte */
+}
+
+/*===========================================================================================================
+ * [Function Name] :
+ * [Description]   :
+ * [Arguments]     : <>      ->
+ *                   <>      ->
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void RFID_sendCommand(uint8 a_command)
+{
+	RFID_writeData(RFID_COMMAND_REG,(a_command & 0X0F));
+}
+
+/*===========================================================================================================
+ * [Function Name] :
+ * [Description]   :
+ * [Arguments]     : <>      ->
+ *                   <>      ->
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void RFID_readCardID(uint8* a_ptr2buffer)
+{
+	for(uint8 digit_counter = 0; digit_counter < RFID_CARD_ID_LENGTH; digit_counter++)
+	{
+		*(a_ptr2buffer + digit_counter) = UART_receiveByte();
+	}
+}
+
+/*===========================================================================================================
+ * [Function Name] :
+ * [Description]   :
+ * [Arguments]     : <>      ->
+ *                   <>      ->
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void RFID_powerDownMode_ON(void)
+{
+	RFID_writeData(RFID_COMMAND_REG,(RFID_NO_CMD_CHANGE_COMMAND)|(1<<4));
+}
+
+/*===========================================================================================================
+ * [Function Name] :
+ * [Description]   :
+ * [Arguments]     : <>      ->
+ *                   <>      ->
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void RFID_powerDownMode_OFF(void)
+{
+	RFID_writeData(RFID_COMMAND_REG,RFID_NO_CMD_CHANGE_COMMAND);
+	/* Wait until it becomes 0 */
+	while(RFID_readData(RFID_COMMAND_REG)&(1<<4));  /* Take care of infinite loop! */
+}
--- HAL/rfid.h
+++ HAL/rfid.h
@@ -1,10 +1,10 @@
 /*
 ===========================================================================================================
 Author       : Mamoun
-Module       : 
+Module       : RFID
 File Name    : rfid.h
 Date Created : Nov 27, 2022
-Description  : 
+Description  : Header file for the RFID driver (using UART).
 ===========================================================================================================
 */
 
@@ -16,13 +16,29 @@ Description  :
                                                < Includes >
 ===========================================================================================================*/
 
-
+#include "../OTHERS/std_types.h"
 
 /*===========================================================================================================
                                 < Definitions and Static Configurations >
 ===========================================================================================================*/
 
+#define RFID_CARD_ID_LENGTH                  (20)
 
+/* RFID Registers List */
+#define RFID_COMMAND_REG                     (0X01)
+#define RFID_TEST_PIN_EN_REG                 (0X33)
+#define RFID_SERIAL_SPEED_REG                (0X1F)
+
+/* RFID Commmand List */
+#define RFID_IDLE_COMMAND                    (0X00)  /* No action, Cancels current command execution. */
+#define RFID_MEM_COMMAND                     (0X01)  /* Stores 25 bytes into the internal buffer.     */
+#define RFID_GENERATE_RANDOM_ID_COMMAND      (0X02)  /* Generates a 10-byte random ID number.         */
+#define RFID_CALC_CRC_COMMAND                (0X03)  /* Activates the CRC coprocessor.                */
+#define RFID_TRANSMIT_COMMAND                (0X04)  /* Transmits data from the FIFO buffer.          */
+#define RFID_NO_CMD_CHANGE_COMMAND           (0X07)  /* No command change.                            */
+#define RFID_RECEIVE_COMMAND                 (0X08)  /* Activates the receiver circuits.              */
+#define RFID_TRANSCEIVE_COMMAND              (0X0C)  /* Transmits data and activates the receiver.    */
+#define RFID_SOFT_RESET_COMMAND              (0X0F)  /* Resets the MFRC522.                           */
 
 /*===========================================================================================================
                                        < Shared Global Variables >
