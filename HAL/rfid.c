2e0b85ef886caba2f19d7431e687c2a68b6cc042
Muhamad Mamoun <muhamadmamoun@gmail.com>
2022-12-31 01:39:38 +0200

    Add RFID driver

--- HAL/rfid.c
+++ HAL/rfid.c
@@ -1,11 +1,11 @@
 /*
-===========================================================================================================
+=============================================================================================================
 Author       : Mamoun
-Module       : RFID
+Module       : RDM6300 RFID
 File Name    : rfid.c
-Date Created : Nov 27, 2022
-Description  : Source file for the RFID driver (using UART).
-===========================================================================================================
+Date Created : Dec 21, 2022
+Description  : Source file for the RDM6300 RFID Reader driver.
+=============================================================================================================
 */
 
 
@@ -14,13 +14,8 @@ Description  : Source file for the RFID driver (using UART).
 ===========================================================================================================*/
 
 #include "rfid.h"
-#include "../MCAL/uart.h"
-
-/*===========================================================================================================
-                                           < Global Variables >
-===========================================================================================================*/
-
-UART_configurationsType UART_configurations = {UART_SINGLE_STOP_BIT,UART_PARITY_DISABLED,9600};
+#include "uart.h"
+#include <stdlib.h>
 
 /*===========================================================================================================
                                           < Functions Definitions >
@@ -28,105 +23,42 @@ UART_configurationsType UART_configurations = {UART_SINGLE_STOP_BIT,UART_PARITY_
 
 /*===========================================================================================================
  * [Function Name] : RFID_init
- * [Description]   : Initialize the RFID module.
+ * [Description]   : Initialize the RFID module by initializing the UART with specific configurations.
  * [Arguments]     : The function takes no arguments.
  * [return]        : The function returns void.
  ==========================================================================================================*/
 void RFID_init(void)
 {
+	UART_configurationsType UART_configurations = {UART_SINGLE_STOP_BIT,UART_PARITY_DISABLED,9600};
 	UART_init(&UART_configurations);
-	RFID_wakeUpDelay();
-	UART_configurations.baudrateValue = 128000;
-	UART_init(&UART_configurations);
-	RFID_powerDownMode_ON();
-	RFID_writeData(RFID_TEST_PIN_EN_REG,0X00); /* Disable MX and DTRQ */
-	RFID_writeData(RFID_SERIAL_SPEED_REG,0X74); /* Change the default BR to 128 KBR */
 }
 
 /*===========================================================================================================
- * [Function Name] : RFID_readData
- * [Description]   : Read the value of a particular register in the RFID module.
- * [Arguments]     : <a_address>      -> the address of the required register.
- * [return]        : The function returns void.
- ==========================================================================================================*/
-uint8 RFID_readData(uint8 a_address)
-{
-	uint8 data;
-	UART_sendByte((a_address & 0X3F)|(1<<7));
-	data = UART_receiveByte();
-	return data;
-}
-
-/*===========================================================================================================
- * [Function Name] :
- * [Description]   :
- * [Arguments]     : <>      ->
- *                   <>      ->
- * [return]        : The function returns void.
- ==========================================================================================================*/
-void RFID_writeData(uint8 a_address, uint8 a_data)
-{
-	UART_sendByte((a_address & 0X3F));
-	UART_sendByte(a_data);
-}
-
-/*===========================================================================================================
- * [Function Name] :
- * [Description]   :
+ * [Function Name] : RFID_readCard
+ * [Description]   : Read the card/tag ID using UART.
  * [Arguments]     : The function takes no arguments.
- * [return]        : The function returns void.
+ * [return]        : The function returns the card/tag ID.
  ==========================================================================================================*/
-void RFID_wakeUpDelay(void)
+sint32 RFID_readCard(void)
 {
-	uint8 data = RFID_readData(0X00); /* Dummy Byte */
-}
+	long card_id = 0;
+	uint8 bytes_counter = 0;
+	uint8 data_buffer[RFID_FRAME_LENGTH] = {'\0'};
+	uint8 card_id_buffer[RFID_ID_LENGTH+1] = {'\0'};
 
-/*===========================================================================================================
- * [Function Name] :
- * [Description]   :
- * [Arguments]     : <>      ->
- *                   <>      ->
- * [return]        : The function returns void.
- ==========================================================================================================*/
-void RFID_sendCommand(uint8 a_command)
-{
-	RFID_writeData(RFID_COMMAND_REG,(a_command & 0X0F));
-}
-
-/*===========================================================================================================
- * [Function Name] : RFID_powerDownMode_ON
- * [Description]   :
- * [Arguments]     : The function takes no arguments.
- * [return]        : The function returns void.
- ==========================================================================================================*/
-void RFID_powerDownMode_ON(void)
-{
-	RFID_writeData(RFID_COMMAND_REG,(RFID_NO_CMD_CHANGE_COMMAND)|(1<<4));
-}
-
-/*===========================================================================================================
- * [Function Name] : RFID_powerDownMode_OFF
- * [Description]   :
- * [Arguments]     : The function takes no arguments.
- * [return]        : The function returns void.
- ==========================================================================================================*/
-void RFID_powerDownMode_OFF(void)
-{
-	RFID_writeData(RFID_COMMAND_REG,RFID_NO_CMD_CHANGE_COMMAND);
-	/* Wait until it becomes 0 */
-	while(RFID_readData(RFID_COMMAND_REG)&(1<<4));  /* Take care of infinite loop! */
-}
-
-/*===========================================================================================================
- * [Function Name] : RFID_readCardID
- * [Description]   : Read the ID of the scanned card/tag.
- * [Arguments]     : <a_ptr2buffer>      -> Pointer to the buffer array to store the ID.
- * [return]        : The function returns void.
- ==========================================================================================================*/
-void RFID_readCardID(uint8* a_ptr2buffer)
-{
-	for(uint8 digit_counter = 0; digit_counter < RFID_CARD_ID_LENGTH; digit_counter++)
+	for(bytes_counter = 0; bytes_counter < RFID_FRAME_LENGTH; bytes_counter++)
 	{
-		*(a_ptr2buffer + digit_counter) = UART_receiveByte();
+		*(data_buffer + bytes_counter) = UART_receiveByte();
 	}
+
+	for(bytes_counter = 0; bytes_counter < RFID_ID_LENGTH; bytes_counter++)
+	{
+		*(card_id_buffer + bytes_counter) = *(data_buffer + bytes_counter + 3);
+	}
+
+	*(card_id_buffer + bytes_counter) = '\0';
+
+	card_id = strtol(card_id_buffer,NULL,16);
+
+	return card_id;
 }
--- HAL/rfid.h
+++ HAL/rfid.h
@@ -1,67 +1,50 @@
 /*
-===========================================================================================================
+=============================================================================================================
 Author       : Mamoun
-Module       : RFID
+Module       : RDM6300 RFID
 File Name    : rfid.h
-Date Created : Nov 27, 2022
-Description  : Header file for the RFID driver (using UART).
-===========================================================================================================
+Date Created : Dec 21, 2022
+Description  : Header file for the RDM6300 RFID Reader driver.
+=============================================================================================================
 */
 
 
-#ifndef HAL_RFID_H_
-#define HAL_RFID_H_
+#ifndef RFID_H_
+#define RFID_H_
 
 /*===========================================================================================================
                                                < Includes >
 ===========================================================================================================*/
 
-#include "../OTHERS/std_types.h"
+#include "std_types.h"
 
 /*===========================================================================================================
                                 < Definitions and Static Configurations >
 ===========================================================================================================*/
 
-#define RFID_CARD_ID_LENGTH                  (20)
-
-/* RFID Registers List */
-#define RFID_COMMAND_REG                     (0x01) /* Starts and stops command execution.            */
-#define RFID_COMIEN_REG                      (0x02) /* Enable/Disable interrupt request control bits. */
-#define RFID_DIVIEN_REG                      (0x03) /* Enable/Disable interrupt request control bits. */
-#define RFID_COMIRQ_REG                      (0x04) /* Interrupt request bits.                        */
-#define RFID_DIVIRQ_REG                      (0x05) /* Interrupt request bits.                        */
-#define RFID_ERROR_REG                       (0x06) /* Error status of the last command executed.     */
-#define RFID_STATUS1_REG                     (0x07) /* Communication status bits.                     */
-#define RFID_STSTUS2_REG                     (0x08) /* Receiver and transmitter status bits.          */
-#define RFID_CONTROL_REG                     (0x0C) /* Miscellaneous control registers.               */
-#define RFID_FIFO_DATA_REG                   (0x09) /* Input and output of 64 byte FIFO buffer.       */
-#define RFID_FIFO_LEVEL_REG                  (0x0A) /* Number of bytes stored in the FIFO buffer.     */
-#define RFID_WATER_LEVEL_REG                 (0x0B) /* Level of FIFO underflow and overflow warning.  */
-#define RFID_BIT_FRAMING_REG                 (0x0D) /* Adjustments for bit-oriented frames.           */
-#define RFID_TEST_PIN_EN_REG                 (0X33) /* Selects the speed of the UART interface.       */
-#define RFID_SERIAL_SPEED_REG                (0X1F) /* Disable the MX and DTRQ signals.               */
-
-/* RFID Commmand List */
-#define RFID_IDLE_COMMAND                    (0X00)  /* No action, Cancels current command execution. */
-#define RFID_MEM_COMMAND                     (0X01)  /* Stores 25 bytes into the internal buffer.     */
-#define RFID_GENERATE_RANDOM_ID_COMMAND      (0X02)  /* Generates a 10-byte random ID number.         */
-#define RFID_CALC_CRC_COMMAND                (0X03)  /* Activates the CRC coprocessor.                */
-#define RFID_TRANSMIT_COMMAND                (0X04)  /* Transmits data from the FIFO buffer.          */
-#define RFID_NO_CMD_CHANGE_COMMAND           (0X07)  /* No command change.                            */
-#define RFID_RECEIVE_COMMAND                 (0X08)  /* Activates the receiver circuits.              */
-#define RFID_TRANSCEIVE_COMMAND              (0X0C)  /* Transmits data and activates the receiver.    */
-#define RFID_SOFT_RESET_COMMAND              (0X0F)  /* Resets the MFRC522.                           */
-
-/*===========================================================================================================
-                                       < User-defined Data Types >
-===========================================================================================================*/
-
-
+#define RFID_FRAME_LENGTH                (14)
+#define RFID_ID_LENGTH                   (8)
 
 /*===========================================================================================================
                                          < Functions Prototypes >
 ===========================================================================================================*/
 
 
+/*===========================================================================================================
+ * [Function Name] : RFID_init
+ * [Description]   : Initialize the RFID module by initializing the UART with specific configurations.
+ * [Arguments]     : The function takes no arguments.
+ * [return]        : The function returns void.
+ ==========================================================================================================*/
+void RFID_init(void);
 
-#endif /* HAL_RFID_H_ */
+
+/*===========================================================================================================
+ * [Function Name] : RFID_readCard
+ * [Description]   : Read the card/tag ID using UART.
+ * [Arguments]     : The function takes no arguments.
+ * [return]        : The function returns the card/tag ID.
+ ==========================================================================================================*/
+sint32 RFID_readCard(void);
+
+#endif /* RFID_H_ */
