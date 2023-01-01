675f51379e91a397882943764dac701dc135c588
Muhamad Mamoun <muhamadmamoun@gmail.com>
2023-01-01 02:16:38 +0200

    Modifications

--- MCAL/timer1.h
+++ MCAL/timer1.h
@@ -24,7 +24,10 @@ Description  : Header file for the AVR Timer1 driver.
 
 typedef enum
 {
-	NORMAL,TOGGLE,NON_INVERTING,INVERTING
+	TIMER1_NORMAL,
+	TIMER1_TOGGLE,
+	TIMER1_NON_INVERTING,
+	TIMER1_INVERTING
 }TIMER1_compareOutputModeType;
 
 typedef enum
