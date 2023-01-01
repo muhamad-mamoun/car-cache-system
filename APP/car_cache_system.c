e40acdab4da1f71105cd4938e3274def2ff72593
Muhamad Mamoun <muhamadmamoun@gmail.com>
2023-01-01 02:17:07 +0200

    APP Layer

--- APP/car_cache_system.c
+++ APP/car_cache_system.c
@@ -1,8 +1,8 @@
 /*
 ===========================================================================================================
-Project      : Car-Cache
-File Name    : main.c
-Date Created : Oct 15, 2022
+Project      : Car-Cache System
+File Name    : car_cache_system.c
+Date Created : Jan 1, 2023
 Description  : Vertical Rotary Car Parking System.
 ===========================================================================================================
 */
@@ -13,7 +13,7 @@ Description  : Vertical Rotary Car Parking System.
                                                < Includes >
 ===========================================================================================================*/
 
-
+#include "car_cache_system.h"
 
 /*===========================================================================================================
                                            < Global Variables >
@@ -40,9 +40,60 @@ Description  : Vertical Rotary Car Parking System.
  ==========================================================================================================*/
 int main(void)
 {
-
-	while(1)
-	{
-
-	}
+	SYSTEM_init();
+	while(1);
 }
+
+void SYSTEM_init(void)
+{
+	ENABLE_GLOBAL_INT();
+
+	LCD_init();
+	LCD_displayStringRowColumn("Car-Cache System",0,2);
+	LCD_displayStringRowColumn("Welcome!",2,6);
+
+	IR_init();
+	RFID_init();
+	BUZZER_init();
+	STEPPER_init();
+
+	LED_init(PORTB_ID,PIN6_ID);
+	LED_init(PORTB_ID,PIN7_ID);
+	LED_init(PORTC_ID,PIN6_ID);
+	LED_init(PORTC_ID,PIN7_ID);
+
+	MEEPROM_voidInit();
+	// read the data from eeprom.
+
+	INT0_init(INT0_RISING_EDGE);
+	INT1_init(INT1_RISING_EDGE);
+	INT2_init(INT2_RISING_EDGE);
+	INT0_setCallBack(SYSTEM_enterCar);
+	INT1_setCallBack(SYSTEM_retrieveCar);
+	INT2_setCallBack(SYSTEM_returnHome);
+
+	_delay_ms(3000);
+	LCD_clearScrean();
+	LCD_displayStringRowColumn("Car-Cache System",0,2);
+	LCD_displayStringRowColumn("» Enter a car.",2,0);
+	LCD_displayStringRowColumn("» Retrieve a car.",3,0);
+}
+
+void SYSTEM_enterCar(void)
+{
+
+}
+
+void SYSTEM_retrieveCar(void)
+{
+
+}
+
+void SYSTEM_returnHome(void)
+{
+
+}
+
+
+
+
